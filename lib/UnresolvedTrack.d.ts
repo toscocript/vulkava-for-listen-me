3e49f382bab46be542c022fa66ff9adc7b40ffbf
Cauã <65617660+toscocript@users.noreply.github.com>
2022-09-27 19:23:00 -0300

    Update UnresolvedTrack.d.ts

--- lib/UnresolvedTrack.d.ts
+++ lib/UnresolvedTrack.d.ts
@@ -9,7 +9,7 @@ export default class UnresolvedTrack {
     readonly source: string;
     requester: unknown;
     private readonly isrc;
-    constructor(vulkava: Vulkava, title: string, author: string, duration?: number, uri?: string, source?: string, isrc?: string);
+    constructor(vulkava: Vulkava, title: string, author: string, duration?: number, uri?: string, source?: string, isrc?: string, special?: string);
     get query(): string;
     build(): Promise<Track>;
     setRequester(requester: unknown): void;
